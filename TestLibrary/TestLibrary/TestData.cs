f2d5f3dbfbd2aa647d954130152d3d640bc9d6c2
Neil Dickerson <ndickerson@dxc.com>
2019-06-12 15:06:52 +0100

    Initial Test Data class with defaults

---
+++ TestLibrary/TestLibrary/TestData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+  public class TestData
+  {
+    public int Key { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public TestData()
+    {
+      Key = 25;
+      Name = "Test Data 1";
+      Description = "Test Default Data Item";
+    }
+  }
+}
